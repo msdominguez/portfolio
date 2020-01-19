3e62835d617b5c6fe163d906ed2dbc5921e60b9e
Maria <shrizzleshreet101@yahoo.com>
2020-01-19 01:06:32 -0500

    debug heroku pt 2

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app = express();
 const router = express.Router();
 const path = require("path");
 
-/*
 router.get("/idm231", function(req, res) {
     res.sendFile(path.join(`${__dirname}/idm231/index.html`));
 });
@@ -13,13 +12,12 @@ router.get("/idm231/detail", function(req, res) {
 });
 
 app.use(express.static("."), router);
-app.listen(process.env.port || 4200);
+app.listen(process.env.PORT || 4200);
 
-
-*/
-
+/*
 var express = require("express");
 var app = express();
 app.use(express.static("."));
 
 app.listen(process.env.PORT || 4200);
+*/
