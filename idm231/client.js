ae5883642210745394e0e1dfb7b6b8c79759b5c1
Maria <shrizzleshreet101@yahoo.com>
2020-02-07 00:16:40 -0500

    update idm

--- idm231/client.js
+++ idm231/client.js
@@ -13,76 +13,108 @@ let zods = [
     "Aqu"
 ];
 
-// make zodiac json objects w all the data i need for ajax
+function isValidDate(d) {
+    return d instanceof Date && !isNaN(d);
+}
 
 function showDetail() {
-    // get input from calendar picker
-    // put into dateToZodiac
-    // do a switch on zods w output of dateToZodiac
-    // case X: replace title w zod info
-    // $('#home').addClass('hidden');
-    // $("#detail-title").html("json data..");
+    let userBirth = new Date($("#birth-input").val());
+
+    if (isValidDate(userBirth)) {
+        let userMonth = userBirth.getMonth() + 1;
+        let userDay = userBirth.getUTCDate();
+
+        let zod = dateToZodiac(userMonth, userDay);
+
+        $.ajax({
+            type: "GET",
+            url: "/idm231/detail",
+            success: function(msg) {
+                populatePage(zod);
+                $("#content").html(msg);
+            }
+        });
+    }
+}
+
+function populatePage(zod) {
+    $.ajax({
+        type: "GET",
+        url: "/getZod",
+        dataType: "json",
+        success: function(msg) {
+            msg.zods.map(val => {
+                if (val.shortname === zod) {
+                    $("#description").html(val.description);
+                    $("#detail-title").html(val.artist);
+                    $("#birthdate").html(val.dob);
+                    $("#detail-img").attr("src", val.img);
+                    $("#caption").html(`${val.title} - ${val.range}`);
+                }
+            });
+        }
+    });
 }
 
 function dateToZodiac(whichMonth, whichDayOfMonth) {
     if (
-        (whichMonth == 12 && whichDayOfMonth >= 22) ||
-        (whichMonth == 1 && whichDayOfMonth <= 19)
+        (whichMonth === 12 && whichDayOfMonth >= 22) ||
+        (whichMonth === 1 && whichDayOfMonth <= 19)
     ) {
         AstroSign = "Cap";
     } else if (
-        (whichMonth == 11 && whichDayOfMonth >= 22) ||
-        (whichMonth == 12 && whichDayOfMonth <= 21)
+        (whichMonth === 11 && whichDayOfMonth >= 22) ||
+        (whichMonth === 12 && whichDayOfMonth <= 21)
     ) {
         AstroSign = "Sag";
     } else if (
-        (whichMonth == 10 && whichDayOfMonth >= 24) ||
-        (whichMonth == 11 && whichDayOfMonth <= 21)
+        (whichMonth === 10 && whichDayOfMonth >= 24) ||
+        (whichMonth === 11 && whichDayOfMonth <= 21)
     ) {
         AstroSign = "Sco";
     } else if (
-        (whichMonth == 9 && whichDayOfMonth >= 23) ||
-        (whichMonth == 10 && whichDayOfMonth <= 23)
+        (whichMonth === 9 && whichDayOfMonth >= 23) ||
+        (whichMonth === 10 && whichDayOfMonth <= 23)
     ) {
         AstroSign = "Lib";
     } else if (
-        (whichMonth == 8 && whichDayOfMonth >= 23) ||
-        (whichMonth == 9 && whichDayOfMonth <= 22)
+        (whichMonth === 8 && whichDayOfMonth >= 23) ||
+        (whichMonth === 9 && whichDayOfMonth <= 22)
     ) {
         AstroSign = "Vir";
     } else if (
-        (whichMonth == 7 && whichDayOfMonth >= 23) ||
-        (whichMonth == 8 && whichDayOfMonth <= 22)
+        (whichMonth === 7 && whichDayOfMonth >= 23) ||
+        (whichMonth === 8 && whichDayOfMonth <= 22)
     ) {
         AstroSign = "Leo";
     } else if (
-        (whichMonth == 6 && whichDayOfMonth >= 22) ||
-        (whichMonth == 7 && whichDayOfMonth <= 22)
+        (whichMonth === 6 && whichDayOfMonth >= 22) ||
+        (whichMonth === 7 && whichDayOfMonth <= 22)
     ) {
         AstroSign = "Can";
     } else if (
-        (whichMonth == 5 && whichDayOfMonth >= 21) ||
-        (whichMonth == 6 && whichDayOfMonth <= 21)
+        (whichMonth === 5 && whichDayOfMonth >= 21) ||
+        (whichMonth === 6 && whichDayOfMonth <= 21)
     ) {
         AstroSign = "Gem";
     } else if (
-        (whichMonth == 4 && whichDayOfMonth >= 20) ||
-        (whichMonth == 5 && whichDayOfMonth <= 20)
+        (whichMonth === 4 && whichDayOfMonth >= 20) ||
+        (whichMonth === 5 && whichDayOfMonth <= 20)
     ) {
         AstroSign = "Tau";
     } else if (
-        (whichMonth == 3 && whichDayOfMonth >= 21) ||
-        (whichMonth == 4 && whichDayOfMonth <= 19)
+        (whichMonth === 3 && whichDayOfMonth >= 21) ||
+        (whichMonth === 4 && whichDayOfMonth <= 19)
     ) {
         AstroSign = "Ari";
     } else if (
-        (whichMonth == 2 && whichDayOfMonth >= 19) ||
-        (whichMonth == 3 && whichDayOfMonth <= 20)
+        (whichMonth === 2 && whichDayOfMonth >= 19) ||
+        (whichMonth === 3 && whichDayOfMonth <= 20)
     ) {
         AstroSign = "Pis";
     } else if (
-        (whichMonth == 1 && whichDayOfMonth >= 20) ||
-        (whichMonth == 2 && whichDayOfMonth <= 18)
+        (whichMonth === 1 && whichDayOfMonth >= 20) ||
+        (whichMonth === 2 && whichDayOfMonth <= 18)
     ) {
         AstroSign = "Aqu";
     }
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 const router = express.Router();
 const path = require("path");
+const zodjson = require(`${__dirname}/idm231/zod.json`);
+app.use(express.static("."), router);
 
 router.get("/idm231", function(req, res) {
     res.sendFile(path.join(`${__dirname}/idm231/index.html`));
@@ -11,5 +13,8 @@ router.get("/idm231/detail", function(req, res) {
     res.sendFile(path.join(`${__dirname}/idm231/detail.html`));
 });
 
-app.use(express.static("."), router);
+app.get("/getZod", function(req, resp) {
+    return resp.json(zodjson);
+});
+
 app.listen(process.env.PORT || 4200);
