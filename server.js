ae9ae1337b528d8d0516f7001223012f0678dbe6
Maria <shrizzleshreet101@yahoo.com>
2021-01-18 13:09:11 -0500

    heroku

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var express = require("express");
 var app = express();
 const router = express.Router();
 const path = require("path");
-const zodjson = require(`${__dirname}/idm231/zod.json`);
+const zodjson = require('./idm231/zod.json');
 app.use(express.static("."), router);
 
 router.get("/idm231", function(req, res) {
-    res.sendFile(path.join(`${__dirname}/idm231/index.html`));
+    res.sendFile(path.join('./idm231/index.html '));
 });
 
 router.get("/idm231/detail", function(req, res) {
-    res.sendFile(path.join(`${__dirname}/idm231/detail.html`));
+    res.sendFile(path.join('./idm231/detail.html'));
 });
 
 app.get("/getZod", function(req, resp) {
