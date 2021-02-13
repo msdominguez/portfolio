517d968e06a1b8635f77a8f09ef6635a2c814b6f
fluorinated <m2015dominguez@gmail.com>
2021-02-13 12:16:28 -0500

    home and nav mobile responsive

--- index.js
+++ index.js
@@ -1,47 +1,23 @@
-function onLoad() {
-    showArtPage();
-}
-
-function showHomePage() {
-    $('.page').hide();
-    $('.main-nav li h3').removeClass('active');
-
-    $('#home').show();
-}
-
-function showAboutPage() {
-    $('.page').hide();
-    $('.nav-link').removeClass('active');
-
-    $('#about').show();
-    $('.main-nav li h3:first').addClass('active');
-}
-
-function showWorkPage() {
-    $('.page').hide();
-    $('.nav-link').removeClass('active');
-
-    $('#work').show();
-    $('li h3:nth-child(1)').addClass('active');
-}
-
 function showProjectsPage() {
-    $('.page').hide();
-    $('.nav-link').removeClass('active');
-    $('.toggle').removeClass('toggle-knob');
-
+    $('#art').hide();
     $('#projects').show();
-    $('.main-nav li h3:nth-last-child(1)').addClass('active');
-    $('.dev-toggle').addClass('toggle-knob');
+    $('.toggle-knob').removeClass('after');
 }
 
 function showArtPage() {
-    $('.page').hide();
-    $('.dev-toggle').addClass('toggle');
-    $('.nav-link').removeClass('active');
-    $('.toggle').removeClass('toggle-knob');
-
+    $('#projects').hide();
     $('#art').show();
-    $('.main-nav li h3:nth-last-child(1)').addClass('active'); // projects
-    $('.art-toggle').addClass('toggle-knob');
+    $('.toggle-knob').addClass('after');
 }
+
+// for css animations
+window.addEventListener(
+    "scroll",
+    () => {
+        document.body.style.setProperty(
+            "--scroll",
+            window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+        );
+    },
+    false
+);
--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const path = require("path");
 const zodjson = require('./idm231/zod.json');
 app.use(express.static("."), router);
 
+router.get("/", function(req, res) {
+    res.sendFile(path.join(`${__dirname}/index.html`));
+});
+
+router.get("/about", function(req, res) {
+    res.sendFile(path.join(`${__dirname}/about.html`));
+});
+
+router.get("/work", function(req, res) {
+    res.sendFile(path.join(`${__dirname}/work.html`));
+});
+
+router.get("/projects", function(req, res) {
+    res.sendFile(path.join(`${__dirname}/projects.html`));
+});
+
 router.get("/idm231", function(req, res) {
     res.sendFile(path.join('./idm231/index.html'));
 });
@@ -16,7 +32,5 @@ router.get("/idm231/detail", function(req, res) {
 app.get("/getZod", function(req, resp) {
     return resp.json(zodjson);
 });
-
-// exports.app = functions.https.onRequest(app);
 
 app.listen(process.env.PORT || 4200);
