95f3c420e9181895502e82e45df547595e24b08a
msdominguez <m2015dominguez@gmail.com>
2022-02-02 21:08:54 -0500

    cleaned up ui, code, scroll nav bar, and responsiveness

--- index.js
+++ index.js
@@ -1,63 +1,36 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAKNNSByd6dTSurAodlIdDZ6c8i5NX89f8",
-    authDomain: "mariasdominguez-d084a.firebaseapp.com",
-    databaseURL: "https://mariasdominguez-d084a.firebaseio.com",
-    projectId: "mariasdominguez-d084a",
-    storageBucket: "mariasdominguez-d084a.appspot.com",
-    messagingSenderId: "381117452410",
-    appId: "1:381117452410:web:a51b43c71e4d91427aebc3",
-    measurementId: "G-9L7V00YCKZ",
+  apiKey: "AIzaSyAKNNSByd6dTSurAodlIdDZ6c8i5NX89f8",
+  authDomain: "mariasdominguez-d084a.firebaseapp.com",
+  databaseURL: "https://mariasdominguez-d084a.firebaseio.com",
+  projectId: "mariasdominguez-d084a",
+  storageBucket: "mariasdominguez-d084a.appspot.com",
+  messagingSenderId: "381117452410",
+  appId: "1:381117452410:web:a51b43c71e4d91427aebc3",
+  measurementId: "G-9L7V00YCKZ",
 };
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-function showProjectsPage() {
-    $("#art").hide();
-    $("#projects").show();
-    $(".toggle-knob").removeClass("after");
-}
+showProjectsPage = () => {
+  $("#art").hide();
+  $("#projects").insertAfter($("nav"));
+  $("#projects").show();
+  $("#toggle").removeClass("art");
+};
 
-function showArtPage() {
-    $("#projects").hide();
-    $("#art").show();
-    $(".toggle-knob").addClass("after");
-}
+showArtPage = () => {
+  $("#projects").hide();
+  $("#art").insertAfter($("nav"));
+  $("#art").show();
+  $("#toggle").addClass("art");
+};
 
-function onClickToggle() {
-    if ($(".toggle-knob").hasClass("after")) {
-        showProjectsPage();
-    } else {
-        showArtPage();
-    }
-}
+toggleProjects = () => {
+  $("#toggle").hasClass("art") ? showProjectsPage() : showArtPage();
+};
 
-// for css animations
-window.addEventListener(
-    "scroll",
-    () => {
-        document.body.style.setProperty(
-            "--scroll",
-            window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-        );
-    },
-    false
-);
-
-function smoothScroll(goTo) {
-    $("html, body").animate({
-            scrollTop: $(goTo).offset().top,
-        },
-        800
-    );
-}
-
-$(window).scroll(function() {
-    $("nav").addClass("nav-sticky");
-    $("body").addClass("sticky-body");
-
-    if ($(window).scrollTop() < 100) {
-        $("nav").removeClass("nav-sticky");
-        $("body").removeClass("sticky-body");
-    }
+$(window).scroll(() => {
+  $(window).scrollTop() === 0 && $("nav").removeClass("nav-sticky");
+  $(window).scrollTop() !== 0 && $("nav").addClass("nav-sticky");
 });
